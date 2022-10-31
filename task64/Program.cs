e3dd03177c0a038235769301b3cb5ead751a39b0
Sandaime-As <asenov1998@mail.ru>
2022-10-31 20:54:44 +0600

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите значение числа N: ");
+int num = Convert.ToInt32(ReadLine());
+WriteLine(PrintNumbers(num));
+
+
+string PrintNumbers(int n)
+{
+    return (n==1)? n.ToString(): n + " "+ PrintNumbers(n-1);
+}
